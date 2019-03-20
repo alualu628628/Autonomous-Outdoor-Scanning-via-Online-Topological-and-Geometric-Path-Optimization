77fec193ff8a7e5063934a030e396a46305911b6
alualu628628 <alualu628628@163.com>
2019-03-20 22:58:55 +0800

    custom husky again

--- husky_gazebo/env-hooks/env_hooks.sh
+++
@@ -1,7 +0,0 @@
-## mallan: This adds our meshes directory to Gazebo's search path, but
-## there has got to be a better way than setting environment variables to do this
-## FIXME: do this without setting environment variables!
-export GAZEBO_MODEL_PATH="$(catkin_find --first-only husky_gazebo worlds):${GAZEBO_MODEL_PATH}"
-
-export GAZEBO_RESOURCE_PATH=
-export GAZEBO_RESOURCE_PATH="$(catkin_find --first-only husky_gazebo worlds):/usr/share/gazebo-2.2:/usr/share/gazebo_models:${GAZEBO_RESOURCE_PATH}"
